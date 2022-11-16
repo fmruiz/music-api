a35ed8c3110dbd21f0572fc708e46663fd1f9e3b
fmruiz <x.francoruiz@gmail.com>
2022-11-16 02:20:35 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+
+/**
+ * Express initialization
+ */
+const app = express();
+const port = process.env.PORT || 8081;
+
+/**
+ * Middlewares
+ */
+app.use(cors());
+
+/**
+ * Server initialization
+ */
+app.listen(port, () => console.log(`Server UP in port ===> ${port}`));
