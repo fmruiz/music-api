ae26b33b3e188b030a98623957fda47748171d6a
fmruiz <x.francoruiz@gmail.com>
2022-12-05 16:57:50 -0300

    add config for mongodb

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const dbConnect = require('./config/mongo');
 
 /**
  * Express initialization
  */
 const app = express();
 const port = process.env.PORT || 8081;
+
+/**
+ * Connect DB
+ */
+dbConnect();
 
 /**
  * Middlewares
